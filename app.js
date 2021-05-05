df8cf1bb50c02dc31ac34fff7077869ae2b19eb7
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2021-05-05 10:32:49 +0200

    add cors handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -6,7 +7,12 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 
 var app = express();
+var corsOptions = {
+  origin: "https://souschef.dev",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
